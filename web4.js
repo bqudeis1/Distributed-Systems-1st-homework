b5b6aa35712874bc9035d1fe209d9654a792c7d3
Baha Qudeisat <43788100+bqudeis1@users.noreply.github.com>
2024-01-27 19:51:33 -0500

    Add files via upload

---
+++ web4.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 2324;
+const serverName = 'Server Four';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`hello world from ${serverName}`);
+
+    // Log the request details to a common file
+    const logEntry = `${new Date().toISOString()} - Request handled by ${serverName} - ${req.method} ${req.url}\n`;
+    fs.appendFile('all_servers.log', logEntry, (err) => {
+        if (err) {
+            console.error(`Error writing to log file: ${err}`);
+        }
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
